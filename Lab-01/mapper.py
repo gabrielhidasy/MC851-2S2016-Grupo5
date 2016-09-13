00657ad9c707691287cc420196062df483d08ff5
Gabriel Hidasy <gabrielhidasy@gmail.com>
2016-09-13 18:01:44 -0300

    Implemented mapper application
    Received the raw data, filters for some pokemon, creates an ID, and
    formats data for sort/reduce

--- Lab-01/mapper.py
+++ Lab-01/mapper.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
+# Mapper application for pokemon filter
+# This mapper filters out pokemons that are not being searched
+
 import sys
 import json
 
+pokemon_target = sys.argv[1].split(",")
 # input comes from STDIN (standard input)
 for line in sys.stdin:
-    # Split the line between epoch and pokemon data
     pokedata_full = json.loads(line)
     pokedata = {}
-    for key in ["pokemon_id", "latitude", "longitude"]:
+    # TODO: Search for pokemon ids (will require table)
+    if pokedata_full["pokemon_name"] not in pokemon_target:
+        continue
+    for key in ["pokemon_id", "latitude", "longitude", "expires"]:
         pokedata[key] = pokedata_full[key]
-    print(pokedata)
+    record_id = (10000*pokedata["pokemon_id"] +
+                 1000*pokedata["latitude"] +
+                 pokedata["longitude"])
+    print("{}\t{}".format(record_id, json.dumps(pokedata)))
