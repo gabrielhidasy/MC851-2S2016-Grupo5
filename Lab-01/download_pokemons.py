b3cfac414261a27d8a1c9f9100378532eb9751f5
Gabriel Hidasy <gabrielhidasy@gmail.com>
2016-09-13 16:22:08 -0300

    Add script to download pokemon data

---
+++ Lab-01/download_pokemons.py
@@ -0,0 +1,56 @@
+import json
+import time
+import urllib.request
+import queue
+import threading
+
+baseURL = "https://skiplagged.com/api/pokemon.php?bounds={},{},{},{}"
+work_queue = queue.Queue(100000)
+file_lock = threading.Lock()
+work_queue_lock = threading.Lock()
+increment = 30
+
+class downloadData(threading.Thread):
+    def __init__(self, work_queue):
+        threading.Thread.__init__(self)
+        self.work_queue = work_queue
+    def run(self):
+        print("Starting thread!")
+        while not work_queue.empty():
+            work_queue_lock.acquire()
+            lon, lat = work_queue.get()
+            print(lon, lat)
+            work_queue_lock.release()
+            URL = baseURL.format(lon/1000,
+                                 (lon+increment)/1000,
+                                 lat/1000,
+                                 (lat+increment)/1000)
+            request_epoch = time.time()
+            response = urllib.request.urlopen(URL)
+            content = response.read()
+            data = json.loads(content.decode("utf8"))
+            file_lock.acquire()
+            try:
+                with open("pokelist.dat", "a+") as pokelist:
+                    for pokemon in data["pokemons"]:
+                        pokemon["epoch"] = request_epoch
+                        pokelist.write("{}\n".format(json.dumps(pokemon)))
+            except KeyError:
+                pass
+            file_lock.release()
+        print("Thread finished!")
+
+
+# Lon and Lat bases on new york (great maps)
+threads = []
+while True:
+    for lon in range(-74100, -73500, increment):
+        for lat in range(40500, 41000, increment):
+            work_queue.put((lon, lat))
+    for _ in range(16):
+        thread = downloadData(work_queue)
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
+    time.sleep(5*60)
