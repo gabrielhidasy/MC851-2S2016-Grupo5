01fa7c6ef52cbc4c9b6ec5f700b3750b5a75339d
Gabriel Hidasy <gabrielhidasy@gmail.com>
2016-09-13 18:03:10 -0300

    Reducer application
    DONE:
            Filters duplicate pokemons
    TODO:
            Separate pokemons in latitude/longitude bins and define output
            format

--- Lab-01/reducer.py
+++ Lab-01/reducer.py
@@ -2,78 +2,42 @@
 
 from operator import itemgetter
 import sys
+import json
 
-from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-maxsize = -5000
-lat_dic = {}
-#for i in range(-128,128):
-#    lat_dic[i] = {}
-    
+
+rpf = {}
+#pokegrid = [[0]*(5000/0.2) for x in range((5000/0.2))]
 # input comes from STDIN
 for line in sys.stdin:
-    # parse the input we got from mapper.py
-    print(line)
-    lats, lons = line.split('\t', 1)
-    # convert count (currently a string) to int
     try:
-        lat = float(lats)
-        lon = float(lons)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        print("PAU")
-        continue
-    print(lat,lon)
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    #coords[lat+127][lon+127] = coords[lat+127][lon+127] + 1
-    try:   
-        lat_dic[(lat,lon)] = lat_dic[(lat,lon)] + 1
-        if(lat_dic[(lat,lon)] > maxsize):
-            maxsize = lat_dic[(lat,lon)]
+        hadoop_id, pokemon_data = line.split("\t")
     except:
-        lat_dic[(lat,lon)] = 1
+        print("Offending line: {}".format(line))
+        continue
 
-print("Reduce step finished, plotting data")
+    try:
+        pokemon_data = json.loads(pokemon_data)
+    except:
+        print("Offending line 2: {}".format(line))
+        print(pokemon_data)
+        continue
 
-def find_max(d):
-    v=list(d.values())
-    k=list(d.keys())
-    return k[v.index(max(v))]
-
-map = Basemap(projection='robin', lat_0=50, lon_0=0,
-              resolution='l', area_thresh=1000.0)
-
-map.drawcoastlines()
-map.drawcountries()
-map.fillcontinents(color='green')
-map.drawmeridians(np.arange(0, 360, 30))
-map.drawparallels(np.arange(-90, 90, 30))
-map.drawmapboundary()
-
-#Choose only k best points to plot
-points = []
-k = 1000
-for i in range(1,k):
-    tmp = find_max(lat_dic)
-    points.append((tmp,lat_dic[tmp]))
-    lat_dic[tmp] = 0
-for p in points:
-    x,y = map(p[0][1],p[0][0])
-    size = max((p[1]/maxsize)*50,3)
-    map.plot(x,y,'bo',markersize=size)
-
-#for key in lat_dic:
-#    x,y = map(key[1],key[0])
-#    size = max(((lat_dic[key]/maxsize)*50),1)
-#    map.plot(x,y,'bo',markersize=size)
-
-plt.show()
-#plt.savefig('map.png')
-print(points)
-#print(maxla,minla,maxlo,minlo)
-#print(lat_dic)
+    if hadoop_id in rpf:
+        dup = False
+        for p in rpf[hadoop_id]:
+            if abs(p-pokemon_data["expires"]) < 200:
+                dup = True
+                break
+        if dup:
+            continue
+    try:
+        rpf[hadoop_id].append(pokemon_data["expires"])
+    except KeyError:
+        rpf[hadoop_id] = [pokemon_data["expires"]]
+    
+    print(pokemon_data)
+ 
