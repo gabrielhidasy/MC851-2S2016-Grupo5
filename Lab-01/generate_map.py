b1d97f976c62b615c09b4c1658d54035b95edf7e
Gabriel Hidasy <gabrielhidasy@gmail.com>
2016-09-13 23:19:57 -0300

    Beautify map generator

--- Lab-01/generate_map.py
+++ Lab-01/generate_map.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python
-import matplotlib.pyplot as plt
-from datetime import datetime
-from mpl_toolkits.basemap import Basemap
-import numpy as np
+import ast
+import matplotlib.pyplot
+import mpl_toolkits.basemap
+import numpy
 import sys
-from ast import literal_eval
 
 # Personally I would use:
 # llcrnrlon=-75.0, llcrnrlat=40.5, urcrnrlon=-72.7, urcrnrlat=41.3,
 
-map = Basemap(projection='mill',
-               resolution='h',
-              llcrnrlon=-75.0,
-              llcrnrlat=40.0,
-              urcrnrlon=-70.0,
-              urcrnrlat=42.0,
-              area_thresh=10000.0)
+map = mpl_toolkits.basemap.Basemap(projection='mill',
+                                   resolution='h',
+                                   llcrnrlon=-75.0,
+                                   llcrnrlat=40.0,
+                                   urcrnrlon=-70.0,
+                                   urcrnrlat=42.0,
+                                   area_thresh=10000.0)
 
 #map.drawcoastlines()
 #map.drawcountries()
 map.shadedrelief()
 #map.fillcontinents(color='green')
-map.drawmeridians(np.arange(0, 360, 0.3))
-map.drawparallels(np.arange(-90, 90, 0.3))
+map.drawmeridians(numpy.arange(0, 360, 0.3))
+map.drawparallels(numpy.arange(-90, 90, 0.3))
 map.drawmapboundary()
 
 for line in sys.stdin:
     try:
         position, size = line.split(":")
         size = int(size)
-        position = literal_eval(position)
+        position = ast.literal_eval(position)
         print(position)
         map.plot(position[2],
                  position[1],
@@ -40,7 +39,7 @@ for line in sys.stdin:
     except ValueError:
         pass
 
-plt.show()
+matplotlib.pyplot.show()
 #plt.savefig('map.png')
 
     
