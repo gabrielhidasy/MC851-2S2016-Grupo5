07ddbd01407b00451a95723b354b00ff6d17d028
Gabriel Hidasy <gabrielhidasy@gmail.com>
2016-09-15 13:33:25 -0300

    Finished map plotter script

--- Lab-01/generate_map.py
+++ Lab-01/generate_map.py
@@ -9,7 +9,7 @@ import sys
 # llcrnrlon=-75.0, llcrnrlat=40.5, urcrnrlon=-72.7, urcrnrlat=41.3,
 
 map = mpl_toolkits.basemap.Basemap(projection='mill',
-                                   resolution='h',
+                                   resolution='f',
                                    llcrnrlon=-75.0,
                                    llcrnrlat=40.0,
                                    urcrnrlon=-70.0,
@@ -24,21 +24,48 @@ map.drawmeridians(numpy.arange(0, 360, 0.3))
 map.drawparallels(numpy.arange(-90, 90, 0.3))
 map.drawmapboundary()
 
+color_list = ["red", "blue", "green", "yellow", "orange", "purple", "black", "white"]
+color_n = 0
+color_table = {}
 for line in sys.stdin:
     try:
         position, size = line.split(":")
         size = int(size)
         position = ast.literal_eval(position)
-        print(position)
-        map.plot(position[2],
-                 position[1],
-                 "rD",
-                 markersize=10,
-                 alpha=0.1,
-                 latlon=True)
+        color = ""
+        do_label = False
+        if position[0] not in name_list:
+            name_list.append(position[0])
+        if position[0] in color_table:
+            color = color_table[position[0]]
+        else:
+            color_table[position[0]] = color_list[color_n]
+            color = color_list[color_n]
+            color_n += 1
+            do_label = True
+            if color_n == len(color_list):
+                color_n = 0
+        if do_label:
+            map.plot(position[2],
+                     position[1],
+                     "D",
+                     color=color,
+                     markersize=10,
+                     alpha=0.8,
+                     latlon=True,
+                     label=position[0])
+            print(color)
+        else:
+            map.plot(position[2],
+                     position[1],
+                     "D",
+                     color=color,
+                     markersize=10,
+                     alpha=0.5,
+                     latlon=True)
     except ValueError:
         pass
-
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
 #plt.savefig('map.png')
 
