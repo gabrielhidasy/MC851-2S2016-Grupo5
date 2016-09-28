9e3f3b3dea2b16c790a480ad2f8ca4cf4530c3ac
Gabriel Hidasy <gabrielhidasy@gmail.com>
2016-09-28 19:11:53 -0300

    Change download feed script to save download time
    We should process posts that are already 2h old, to get more data

--- Lab-02/download_feed.py
+++ Lab-02/download_feed.py
@@ -3,21 +3,17 @@ import time
 import urllib.request
 
 URL = "http://www.15minutesoffame.be/9gag/rss/9GAG_-_Fresh.atom"
-BASE_FILE_NAME = "9gag"
-FILE_EXT = ".dat"
+BASE_FILE_NAME = "9gag-{}.dat"
 
 i = 0
-
 while True:
     response = urllib.request.urlopen(URL)
     content = response.read()
     e = ET.fromstring(content)
 
     print("Iteration {}".format(i))
-
-    file_name = BASE_FILE_NAME + str(i) + FILE_EXT
-    
-    i = i+1
+    i = i + 1
+    file_name = BASE_FILE_NAME.format(int(time.time()))
     
     try:
         with open(file_name, "a+") as gag:
