2affe32a258c8dec7a582208f529808f91be1a9e
Gabriel Hidasy <gabrielhidasy@gmail.com>
2016-10-01 15:20:14 -0300

    Update download feed to avoid duplicates

--- Lab-02/download_feed.py
+++ Lab-02/download_feed.py
@@ -3,9 +3,10 @@ import time
 import urllib.request
 
 URL = "http://www.15minutesoffame.be/9gag/rss/9GAG_-_Fresh.atom"
-BASE_FILE_NAME = "9gag-{}.dat"
+BASE_FILE_NAME = "post_lists/new/9gag-{}.dat"
 
 i = 0
+seen_urls = {}
 while True:
     response = urllib.request.urlopen(URL)
     content = response.read()
@@ -14,16 +15,18 @@ while True:
     print("Iteration {}".format(i))
     i = i + 1
     file_name = BASE_FILE_NAME.format(int(time.time()))
-    
+
     try:
         with open(file_name, "a+") as gag:
             for child in e:
                 if "entry" in child.tag:
                     for entry in child:
                         if "link" in entry.tag:
-                            gag.write("{}\n".format(entry.attrib["href"]))
+                            if entry.attrib["href"] not in seen_urls:
+                                seen_urls[entry.attrib["href"]] = 1
+                                gag.write("{}\n".format(entry.attrib["href"]))
     except KeyError:
         pass
 
     time.sleep(2*60)
-    
+
